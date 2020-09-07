bed75f7f172c7a5ac8b68a58272332c8743fa0b1
Kajanth Kumar <kajanthkumar@gmail.com>
2020-09-06 20:05:10 -0400

    Updates

---
+++ precache-manifest.a005f76b27b8a7d340ff7fa6a83974f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff6a69a5728ba813425752e249dddf9",
+    "url": "/repo-name/index.html"
+  },
+  {
+    "revision": "a21ef43f42308071d462",
+    "url": "/repo-name/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "996db446a67269397d60",
+    "url": "/repo-name/static/css/main.23e0943c.chunk.css"
+  },
+  {
+    "revision": "a21ef43f42308071d462",
+    "url": "/repo-name/static/js/2.09a03e6a.chunk.js"
+  },
+  {
+    "revision": "be48727424509191744560c94f17de24",
+    "url": "/repo-name/static/js/2.09a03e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996db446a67269397d60",
+    "url": "/repo-name/static/js/main.2e95756e.chunk.js"
+  },
+  {
+    "revision": "165103175873f932d6af",
+    "url": "/repo-name/static/js/runtime-main.b4f92e58.js"
+  },
+  {
+    "revision": "625d1aff2d58f024870ef61b95680166",
+    "url": "/repo-name/static/media/noImageAvailable.625d1aff.jpg"
+  }
+]);
