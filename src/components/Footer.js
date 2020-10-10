df08498ab5bdbf01c82467a325e0e17ef7df6512
Kajanth Kumar <kajanthkumar@gmail.com>
2020-10-10 16:23:28 -0400

    saving work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="flexParent">
       <p>
-        © 2020 Submission for Shopify Winter 2021 Internship. Designed and Coded
+        © 2020 Designed and Coded
         by:
         <a href="https://kajanthkumar.com/" target="blank">
           Kajanth Kumar
